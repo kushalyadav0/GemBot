cb1285d225fc824137eaa396712836f627c8445c
kushalyadav0 <ky1368177@gmail.com>
2025-07-26 15:08:29 +0530

    Update core app models, migrations, admin, and templates with UI improvisation
    - Modified core/settings.py and core_app/models.py for improved configuration and data handling
    - Updated core_app/admin.py for better admin interface management
    - Added migration 0002_alter_chat_timestamp_customuser.py to adjust chat timestamp and user model
    - Refined templates/base.html and templates/index.html for enhanced UI and UI improvisation

--- core_app/models.py
+++ core_app/models.py
@@ -1,15 +1,61 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth import get_user_model
-
-User = get_user_model()
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
 # Create your models here.
-class Chat(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='gembot_messages') # we can do with related_name: user.gembot_messages.all() to get all messages by that user.
-    question = models.TextField()
-    answer = models.TextField()
-    timestamp = models.DateField(auto_now_add=True)
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+                raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, verbose_name="Email address")
+    username = models.CharField(max_length=255, blank=True, null=True, verbose_name="Username")
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    date_joined = models.DateTimeField(auto_now_add=True)
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [] # Add other required fields if any, besides the USERNAME_FIELD
 
     def __str__(self):
-        return f"(self.email): {self.question[:20]}"
+        return self.email
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        ordering = ['-date_joined']
+
+class Chat(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='gembot_messages'
+        ) # we can do with related_name: user.gembot_messages.all() to get all messages by that user.
+    question = models.TextField()
+    answer = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"({self.user.email}): {self.question[:20]}"
  
